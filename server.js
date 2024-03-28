10f9d90cbc697c2dddb265bd62e3505d1ad1b23d
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-03-28 14:14:59 +0500

    change

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  console.log("Here");
+  // res.send("Hi Sohibjon");
+  // res.sendStatus(202);
+  res.render("index", { text: "Welcome to Tashkent"});
+})
+app.listen(3000);
 
